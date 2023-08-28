75c80d150e9bc9cff2ad90902df4bf18e8777db9
William da Conceição Pereira <williamcpdw014@gmail.com>
2023-08-28 08:39:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const show_slide_bnt = document.querySelector("button#show_slide"); // o botão que mostra o slide
+
+function show_slide() {
+    //alert("test");
+}
+
+// eventos
+
+show_slide_bnt.addEventListener("click", show_slide); // atribui o evento show slide ao botão
