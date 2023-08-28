15240a4e810581b8a559f7cdc3ca9a6f81814535
William da Conceição Pereira <williamcpdw014@gmail.com>
2023-08-28 20:57:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const show_slide_bnt = document.querySelector("button#show_slide"); // o botão que mostra o slide
+const slide_box = document.querySelector("div#slide_box"); // a box dos slides
 
-function show_slide() {
-    //alert("test");
+const show_slide_bnt = document.querySelector("button#show_slide"); // o botão que mostra o slide
+const hide_slide_bnt = document.querySelector("button#close_slide_box"); // botão que esconde o slide
+
+function show_slide(visibility) { // faz aparecer os slides (true se devem aparecer)
+    slide_box.style.opacity = visibility? 1 : 0;
+
+    setTimeout(() => slide_box.style.visibility = visibility? "visible" : "hidden", visibility? 0 : 400);
 }
 
 // eventos
 
-show_slide_bnt.addEventListener("click", show_slide); // atribui o evento show slide ao botão
+show_slide_bnt.addEventListener("click", () => show_slide(true)); // atribui o evento show slide false ao botão
+hide_slide_bnt.addEventListener("click", () => show_slide(false)); // atribui o evento show sllide true ao botão
